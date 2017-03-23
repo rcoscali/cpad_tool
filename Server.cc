bf5e6fbd84e1197847be3df09325fd5481550ae9
Rémi Cohen-Scali <remi.cohen-scali@laposte.net>
2017-03-23 08:42:55 +0100

    Added forking daemon server implem and protobuf version req/resp
    Created a VersionRequest message from data received
    Create a VersionResponse mesage to send back
    Added some extra traces
    Added signal handler implem for SIGCHLD
    Added fork for handling session in a new process

--- Server.cc
+++ Server.cc
@@ -1,19 +1,18 @@
 //
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
-// http://www.boost.org/doc/libs/1_53_0/doc/html/boost_asio/examples.html
-// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include <cstdlib>
 #include <iostream>
 
 #include "Server.h"
+#include "plugin_request.pb.h"
 
-using namespace std;
 using namespace boost::system;
 using namespace boost;
 using namespace boost::asio;
@@ -45,12 +44,26 @@ cpad::session::handle_read(const error_code& error,
 			   size_t bytes_transferred)
 {
   if (!error)
-    async_write(m_socket,
-		buffer(m_data, bytes_transferred),
-		boost::bind(&session::handle_write,
-			    this,
-			    asio::placeholders::error));
-
+    {
+      std::cout << "handle_read" << std::endl;
+      VersionRequest req;
+      req.ParseFromString(m_data);
+      std::cout << "client minor: " << req.client_version_minor() << std::endl;
+      std::cout << "client major: " << req.client_version_major() << std::endl;
+      std::cout << "client provider: " << req.client_provider_name() << std::endl;
+      std::string response;
+      VersionResponse resp;
+      resp.set_server_version_minor(3);
+      resp.set_server_version_major(4);
+      resp.set_server_provider_name("server");
+      resp.SerializeToString(&response);
+      response.copy(m_data, response.length());
+      async_write(m_socket,
+                  buffer(m_data, response.length()),
+                  boost::bind(&session::handle_write,
+                              this,
+                              asio::placeholders::error));
+    }
   else
     delete this;
 }
@@ -59,11 +72,15 @@ void
 cpad::session::handle_write(const error_code& error)
 {
   if (!error)
-    m_socket.async_read_some(buffer(m_data, max_length),
-			     boost::bind(&session::handle_read,
-					 this,
-					 asio::placeholders::error,
-					 asio::placeholders::bytes_transferred));
+    {
+      std::cout << "handle_write" << std::endl;
+      m_socket.async_read_some(buffer(m_data, max_length),
+                               boost::bind(&session::handle_read,
+                                           this,
+                                           asio::placeholders::error,
+                                           asio::placeholders::bytes_transferred));
+      std::cout << "data written: " << m_data << std::endl;
+    }
   else
     delete this;
 }
@@ -72,9 +89,29 @@ cpad::session::handle_write(const error_code& error)
 
 cpad::server::server(io_service& io_svc, short port)
   : m_io_svc(io_svc),
+    m_signal(io_svc, SIGCHLD),
     m_acceptor(io_svc, tcp::endpoint(tcp::v4(), port))
 {
   start_accept();
+}
+
+void
+cpad::server::start_signal_wait(void)
+{
+  m_signal.async_wait(boost::bind(&server::handle_signal_wait, this));
+}
+
+void
+cpad::server::handle_signal_wait(void)
+{
+  if (m_acceptor.is_open())
+    {
+      // Reap completed child processes so that we don't end up with zombies.
+      int status = 0;
+      while (waitpid(-1, &status, WNOHANG) > 0) {}
+
+      start_signal_wait();
+    }
 }
 
 void
@@ -84,7 +121,7 @@ cpad::server::start_accept()
   m_acceptor.async_accept(new_session->socket(),
 			  boost::bind(&server::handle_accept,
 				      this,
-				      new_session,
+                                      new_session,
 				      asio::placeholders::error));
 }
 
@@ -93,7 +130,20 @@ cpad::server::handle_accept(session* new_session,
 			    const error_code& error)
 {
   if (!error)
-    new_session->start();
+    {
+      std::cout << "Forking for handling session\n";
+      m_io_svc.notify_fork(io_service::fork_prepare);
+      if (fork() > 0)
+        {
+          m_io_svc.notify_fork(io_service::fork_child);
+          m_acceptor.close();
+          m_signal.cancel();
+
+          std::cout << "PID " << getpid() << " forked for handling session\n";
+          
+          new_session->start();
+        }
+    }
 
   else
     delete new_session;
