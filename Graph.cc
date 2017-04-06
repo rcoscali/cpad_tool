94a2b36cbabad684f7f7fd3cdf081e00cd8ca571
Rémi Cohen-Scali <remi.cohen-scali@laposte.net>
2017-04-06 12:30:42 +0200

    Added utilities for apex allocation
    Added entry point mngt and dot dump
    Added initial checkpoint support
    Added mngt of all nodes in graph (for allocation algo support)

--- Graph.cc
+++ Graph.cc
@@ -6,13 +6,17 @@
 
 using namespace std;
 
-cpad::Graph::Graph(uuid_t grid)
+cpad::Graph::Graph(uuid_t grid, int start_checkpoint)
+  : m_entry(nullptr),
+    _has_entry(false),
+    m_cunits_ptr(),
+    m_checkpoint(start_checkpoint)
 {
   uuid_copy(m_grid, grid);
 }
 
-cpad::Graph::Graph()
-  : cpad::Graph((unsigned char *)INVALID_GRID)
+cpad::Graph::Graph(int start_checkpoint)
+  : cpad::Graph((unsigned char *)NULL_GRID, start_checkpoint)
 {
   uuid_generate(m_grid);
 }
@@ -22,15 +26,34 @@ cpad::Graph::~Graph()
 }
 
 cpad::Graph::Graph(Graph const& a_graph_to_copy)
-  : Graph((unsigned char *)a_graph_to_copy.m_grid)
+  : Graph((unsigned char *)a_graph_to_copy.m_grid, a_graph_to_copy.m_checkpoint)
 {
+  m_entry = a_graph_to_copy.m_entry;
+  _has_entry = a_graph_to_copy._has_entry;
+  m_cunits_ptr = a_graph_to_copy.m_cunits_ptr;
 }
 
 cpad::Graph&
 cpad::Graph::operator = (cpad::Graph const& a_graph_to_affect)
 {
   uuid_copy(m_grid, a_graph_to_affect.m_grid);
+  m_checkpoint = a_graph_to_affect.m_checkpoint;
+  m_entry = a_graph_to_affect.m_entry;
+  _has_entry = a_graph_to_affect._has_entry;
+  m_cunits_ptr = a_graph_to_affect.m_cunits_ptr;
+
   return (*this);
+}
+
+const char *
+cpad::Graph::get_name(const char **out)
+{
+  if (out == (const char **)NULL)
+    throw std::invalid_argument("Invalid NULL pointer argument for returning name");
+  if (uuid_is_null(m_grid))
+    throw std::logic_error("Graph has a null graph identifier");
+  uuid_unparse_upper(m_grid, (char *)*out);
+  return (*out);
 }
 
 void
@@ -42,13 +65,40 @@ cpad::Graph::add_cunit(shared_ptr<cpad::CUnit> a_cunit)
 void
 cpad::Graph::add_cunit(cpad::CUnit *&a_cunit)
 {
-  m_cunits_ptr.push_back(shared_ptr<cpad::CUnit>(a_cunit));
+  add_cunit(shared_ptr<cpad::CUnit>(a_cunit));
 }
 
 vector<shared_ptr<cpad::CUnit>>
 cpad::Graph::get_cunits(void)
 {
   return m_cunits_ptr;
+}
+
+void
+cpad::Graph::set_entry(shared_ptr<cpad::CUnit> an_entry)
+{
+  if (_has_entry)
+    throw std::logic_error("Graph already has an entry point");
+  m_entry = an_entry;
+  _has_entry = true;
+}
+
+void
+cpad::Graph::set_entry(cpad::CUnit *&an_entry)
+{
+  set_entry(shared_ptr<cpad::CUnit>(an_entry));
+}
+
+bool
+cpad::Graph::has_entry(void)
+{
+  return _has_entry;
+}
+
+shared_ptr<cpad::CUnit>
+cpad::Graph::get_entry(void)
+{
+  return m_entry;
 }
 
 void
@@ -70,9 +120,23 @@ cpad::Graph::dump(std::ostream &os)
   os << "    labeldistance=30;\n";
   os << "    labelangle=45;\n";
   os << "    pencolor=\"#40FF40\";\n";
-  os << "    label=\"Compilation Graph ID: ";
-  os << uuid << "\";\n";
+  os << "    label=\"Compilation Graph ID: " << uuid << "\";\n";
   os << "    labeljust=r;\n";
+  if (_has_entry)
+    {
+      os << "    comment=\"Entry point is in compilation unit '" << get_entry()->get_filename() << "'\";\n";
+      if (get_entry()->has_entry())
+        {
+          os << "    comment=\"and in function '" << get_entry()->get_entry()->get_name() << "'\";\n";
+          if (get_entry()->get_entry()->has_entry())
+            {
+              os << "    comment=\"and in basic_block '" << get_entry()->get_entry()->get_entry()->get_name() << "'\";\n";
+            }
+        }
+      os << "    \n";
+      os << "    ENTRY [shape=circle,style=filled,fontcolor=white,color=black,fixedsize=true,fontsize=8,width=0.4,label=\"Entry\"];\n";
+      os << "    \n";
+    }
   for (vector<shared_ptr<CUnit>>::iterator it = m_cunits_ptr.begin();
        it != m_cunits_ptr.end();
        it++)
@@ -80,5 +144,82 @@ cpad::Graph::dump(std::ostream &os)
       shared_ptr<CUnit> cunit = *it;
       (*cunit).dump(os);
     }
+
+  cerr << "has entry?: " << _has_entry << "\n";
+  cerr << "entry: " << get_entry() << "\n";
+  cerr << "entry has entry?: " << get_entry()->has_entry() << "\n";
+  cerr << "entry(entry): " << get_entry()->get_entry() << "\n";
+  cerr << "entry(entry) has entry?: " << get_entry()->get_entry()->has_entry() << "\n";
+  if (_has_entry && get_entry()->has_entry() && get_entry()->get_entry()->has_entry())
+    {
+      Func entry_func = *(get_entry()->get_entry());
+      Node entry_node = *(get_entry()->get_entry()->get_entry());
+      os << "    \n";
+      os << "    ENTRY -> " << entry_node.get_cluster_name() << "[style=\"dotted,bold\",color=red,weight=3,constraint=false,decorate=false];\n";
+      os << "    \n";
+    }
   os << "}\n";
+}
+
+int
+cpad::Graph::get_current_checkpoint()
+{
+  return m_checkpoint;
+}
+
+void
+cpad::Graph::add_node(const shared_ptr<cpad::Node> &a_shared_node_ptr)
+{
+  add_node(weak_ptr<cpad::Node>(a_shared_node_ptr));
+}
+
+void
+cpad::Graph::add_node(const weak_ptr<cpad::Node> &a_weak_node_ptr)
+{
+  m_all_nodes.push_back(a_weak_node_ptr);
+}
+
+void
+cpad::Graph::add_node(cpad::Node * const &a_node_ptr)
+{
+  add_node(weak_ptr<cpad::Node>(shared_ptr<cpad::Node>(a_node_ptr)));
+}
+
+vector<weak_ptr<cpad::Node>>
+cpad::Graph::get_all_nodes(void)
+{
+  return m_all_nodes;
+}
+
+void
+cpad::Graph::allocate_cfi_apex(void)
+{
+  cerr << "Starting APEX allocation: current checkpoint: " << get_current_checkpoint() << "\n";
+
+  cerr << "All nodes :";
+  for (vector<weak_ptr<Node>>::iterator it = m_all_nodes.begin();
+       it != m_all_nodes.end();
+       it++)
+    {
+      weak_ptr<cpad::Node> wnode = *it;
+      shared_ptr<cpad::Node> node_ptr;
+      if (node_ptr = wnode.lock())
+        cerr << " " << node_ptr->get_name();
+    }
+  cerr << "\n";
+  
+  cerr << "Computing ancestors ...\n";
+  for (vector<weak_ptr<Node>>::iterator it = m_all_nodes.begin();
+       it != m_all_nodes.end();
+       it++)
+    {
+      weak_ptr<cpad::Node> wnode = *it;
+      shared_ptr<cpad::Node> node_ptr;
+      if (node_ptr = wnode.lock())
+        {
+          cerr << "For node " << node_ptr->get_name() << ":\n";
+          node_ptr->compute_ancestors();
+          node_ptr->dump_ancestors();
+        }
+    }
 }
--- Graph.h
+++ Graph.h
@@ -12,6 +12,7 @@
 #include <vector>
 
 #include "CUnit.h"
+#include "Node.h"
 
 using namespace std;
 
@@ -29,26 +30,46 @@ namespace cpad
               0xff, 0xff, 0xff, 0xff );
 
   class CUnit;
+  class Node;
   
   class Graph
   {
   public:
-    Graph(uuid_t grid);
-    Graph();
+    Graph(uuid_t, int);
+    Graph(int);
     virtual ~Graph();
     Graph(Graph const&);
     Graph& operator = (Graph const&);
+
+    const char *get_name(const char **);
 
     void add_cunit(shared_ptr<CUnit>);
     void add_cunit(CUnit *&);
     vector<shared_ptr<CUnit>> get_cunits(void);
 
+    void set_entry(shared_ptr<CUnit>);
+    void set_entry(CUnit *&);
+    bool has_entry(void);
+    shared_ptr<CUnit> get_entry(void);
+
     void dump(ostream &);
+
+    int get_current_checkpoint(void);
+    void add_node(const shared_ptr<Node> &);
+    void add_node(const weak_ptr<Node> &);
+    void add_node(Node * const&);
+    vector<weak_ptr<Node>> get_all_nodes(void);
+
+    void allocate_cfi_apex(void);
 
   private:
 
+    int m_checkpoint;
+    shared_ptr<CUnit> m_entry;
+    bool _has_entry;
     vector<shared_ptr<CUnit>> m_cunits_ptr;
     uuid_t m_grid;
+    vector<weak_ptr<Node>> m_all_nodes;
     
   }; // class Graph
 
