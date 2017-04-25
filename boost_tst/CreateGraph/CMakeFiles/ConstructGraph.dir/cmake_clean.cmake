4e869f66c2052796daa0967d9c181ce5b2818c0b
Rémi Cohen-Scali <remi.cohen-scali@nagra.com>
2017-04-25 16:08:41 +0200

    Removed junk

--- boost_tst/CreateGraph/CMakeFiles/ConstructGraph.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ConstructGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- boost_tst/CreateGraph/CMakeFiles/ConstructGraph.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ConstructGraph.
-# This may be replaced when dependencies are built.
