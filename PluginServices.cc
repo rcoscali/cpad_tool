a8e04a74e1e78ca7c7ec503051cd5bf2bb6715e2
Rémi Cohen-Scali <rcoscali@gmail.com>
2017-06-02 16:28:31 +0200

    Client/Server test implems
    Added a gRPC test client implem
    Added a gRPC test server implem

--- PluginServices.cc
+++ PluginServices.cc
@@ -1,6 +1,10 @@
 //
 // Copyright 2017, Google Inc.
 //
+#include <iostream>
+#include <sstream>
+
+#include <boost/program_options.hpp>
 
 #include <grpc/grpc.h>
 #include <grpc++/server.h>
@@ -12,6 +16,8 @@
 #include "InsertionPointMsg.h"
 #include "plugin_request.grpc.pb.h"
 
+namespace po = boost::program_options;
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -19,6 +25,10 @@ using grpc::ServerReader;
 using grpc::ServerReaderWriter;
 using grpc::ServerWriter;
 using grpc::Status;
+
+static unsigned char verbose_option = 0;
+static char *hostname_option = (char *)"localhost";
+static unsigned int port_option = 50051;
 
 class PluginServicesImpl final : public ::cpad::insns::PluginServices::Service
 {
@@ -36,12 +46,15 @@ class PluginServicesImpl final : public ::cpad::insns::PluginServices::Service
                    const ::cpad::insns::VersionRequest* request,
                    ::cpad::insns::VersionResponse* response)
   {
+    std::cout << "---===[> Client sent:" << std::endl;
     ::cpad::insns::VersionRequestHelper vrh(request);
     vrh.dump();
     
     response->set_server_version_minor(0);
     response->set_server_version_major(1);
-    response->set_server_provider_name(std::string("GRPC test server"));
+    response->set_server_provider_name(std::string("gRPC test server"));
+
+    std::cout << "---===[> Server respond:" << std::endl;
     ::cpad::insns::VersionResponseHelper vresph((const ::cpad::insns::VersionResponse*)response);
     vresph.dump();
     
@@ -53,11 +66,14 @@ class PluginServicesImpl final : public ::cpad::insns::PluginServices::Service
                           const ::cpad::insns::InsertionPointRequest* request,
                           ::cpad::insns::InsertionPointResponse* response)
   {
+    std::cout << "---===[> Client sent:" << std::endl;
     ::cpad::insns::InsertionPointRequestHelper iph(request);
     iph.dump();
     
     response->set_insert_asm_statement(true);
     response->set_asm_statement(std::string("__asm(Nop; Nop; Nop)"));
+
+    std::cout << "---===[> Server respond:" << std::endl;
     ::cpad::insns::InsertionPointResponseHelper ipresph((const ::cpad::insns::InsertionPointResponse*)response);
     ipresph.dump();
     return ::Status::OK;
@@ -67,9 +83,8 @@ class PluginServicesImpl final : public ::cpad::insns::PluginServices::Service
   
 };
 
-void RunServer(void)
+void RunServer(std::string server_address)
 {
-  std::string server_address("0.0.0.0:50051");
   PluginServicesImpl *service = new PluginServicesImpl();
 
   ServerBuilder builder;
@@ -82,7 +97,54 @@ void RunServer(void)
 
 int main(int argc, char** argv)
 {
-  RunServer();
+  // Check PB compat
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  // Declare the supported options.
+  po::options_description desc("Options allowed for test gRPC PluginServices server.");
+  desc.add_options()
+    ("help,h", "Produce help message.")
+    ("verbose,v", po::value<unsigned char>(&verbose_option)->default_value(0)->implicit_value(1), "Set verbosity level.")
+    ("hostname", po::value<std::string>()->default_value("localhost"), "Set server hostname. Default: localhost.")
+    ("port,p", po::value<unsigned int>(&port_option)->default_value(50051), "Set server port. Default: 50051.")
+    ;
+
+  po::variables_map vm;
+  
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+    {
+      std::cout << desc << std::endl;
+    }
+  else
+    {
+      if (vm.count("hostname"))
+        hostname_option = (char *)vm["hostname"].as<std::string>().c_str();
+
+      if (vm.count("port"))
+        port_option = vm["port"].as<unsigned int>();
+      
+      if (verbose_option)
+        {
+          std::cout << "**> Verbosity set to level " << (unsigned int)verbose_option << std::endl;
+          if (vm.count("hostname"))
+            std::cout << "**> Hostname set to '" << vm["hostname"].as<std::string>() << "'" << std::endl;
+          if (vm.count("port"))
+            std::cout << "**> Port set to " << vm["port"].as<unsigned int>() << std::endl;
+        }
+
+      std::ostringstream server_os;
+      server_os << hostname_option << ":" << port_option;
+      std::string server_address_str = server_os.str();
+
+      if (verbose_option)
+        std::cout << "**> Running server on '" << server_address_str << "'" << std::endl;
+
+      RunServer(server_address_str);
+    }
 
   return 0;
 }
+
--- PluginServicesClient.cc
+++ PluginServicesClient.cc
@@ -3,6 +3,9 @@
 //
 
 #include <iostream>
+#include <sstream>
+
+#include <boost/program_options.hpp>
 
 #include <grpc/grpc.h>
 #include <grpc++/channel.h>
@@ -13,6 +16,8 @@
 #include "VersionMsg.h"
 #include "InsertionPointMsg.h"
 #include "plugin_request.grpc.pb.h"
+
+namespace po = boost::program_options;
 
 using grpc::Channel;
 using grpc::ClientContext;
@@ -31,6 +36,10 @@ using cpad::insns::InsertionPointRequestHelper;
 using cpad::insns::InsertionPointResponse;
 using cpad::insns::InsertionPointResponseHelper;
 
+static unsigned char verbose_option = 0;
+static char *hostname_option = (char *)"localhost";
+static unsigned int port_option = 50051;
+
 class PluginServicesClient
 {
 public:
@@ -46,7 +55,7 @@ public:
     VersionRequestHelper vrh(0, 1, std::string("gRPC test Client"));
     VersionResponseHelper vresph(0, 0, std::string(""));
 
-    std::cout << "Client send:" << std::endl;
+    std::cout << "---===[> Client send:" << std::endl;
     vrh.dump();
     
     Status status = m_stub->VersionService(&context,
@@ -54,11 +63,11 @@ public:
                                            (VersionResponse*)&vresph);
     if (!status.ok())
       {
-        std::cout << "VersionService rpc failed." << std::endl;
+        std::cout << "---EEE* VersionService rpc failed." << std::endl;
       }
     else
       {
-        std::cout << "Server responded:" << std::endl;
+        std::cout << "---===[> Server responded:" << std::endl;
         vresph.dump();
       }
   }
@@ -71,7 +80,7 @@ public:
                                      cpad::insns::FUNCTION_BEFORE_CALL);
     InsertionPointResponseHelper ipresph("");
 
-    std::cout << "Client send:" << std::endl;
+    std::cout << "---===[> Client send:" << std::endl;
     iprh.dump();
 
     Status status = m_stub->InsertionPointService(&context,
@@ -79,11 +88,11 @@ public:
                                                   (InsertionPointResponse*)&ipresph);
     if (!status.ok())
       {
-        std::cout << "InsertionPointService rpc failed." << std::endl;
+        std::cout << "---EEE* InsertionPointService rpc failed." << std::endl;
       }
     else
       {
-        std::cout << "Server responded:" << std::endl;
+        std::cout << "---===[> Server responded:" << std::endl;
         ipresph.dump();
       }
     
@@ -97,13 +106,62 @@ private:
 int
 main(int argc, char** argv)
 {
-  PluginServicesClient client(::grpc::CreateChannel("localhost:50051",
-                                                    grpc::InsecureChannelCredentials()));
+  // Check PB compat
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  std::cout << "**[ VersionService RPC test:" << std::endl;
-  client.VersionService();
-  std::cout << "**[ InsertionPointService RPC test:" << std::endl;
-  client.InsertionPointService();
+  // Declare the supported options.
+  po::options_description desc("Options allowed for test gRPC PluginServices client.");  
+  desc.add_options()
+    ("help,h", "Produce help message.")
+    ("verbose,v", po::value<unsigned char>(&verbose_option)->default_value(0)->implicit_value(1), "Set verbosity level.")
+    ("hostname", po::value<std::string>()->default_value("localhost"), "Set server hostname. Default: localhost.")
+    ("port,p", po::value<unsigned int>(&port_option)->default_value(50051), "Set server port. Default: 50051.")
+    ;
+  po::positional_options_description pdesc;
+  pdesc.add("hostname", 1);
+
+  po::variables_map vm;
+  
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+    {
+      std::cout << desc << std::endl;
+    }
+  else
+    {
+      if (vm.count("hostname"))
+        hostname_option = (char *)vm["hostname"].as<std::string>().c_str();
+
+      if (vm.count("port"))
+        port_option = vm["port"].as<unsigned int>();
+      
+      if (verbose_option)
+        {
+          std::cout << "**> Verbosity set to level " << (unsigned int)verbose_option << std::endl;
+          if (vm.count("hostname"))
+            std::cout << "**> Hostname set to '" << vm["hostname"].as<std::string>() << "'" << std::endl;
+          if (vm.count("port"))
+            std::cout << "**> Port set to " << vm["port"].as<unsigned int>() << std::endl;
+        }
+
+      std::ostringstream server_os;
+      server_os << hostname_option << ":" << port_option;
+      std::string server_address_str = server_os.str();
+
+      if (verbose_option)
+        std::cout << "**> Connecting to server on '" << server_address_str << "'" << std::endl;
+
+
+      PluginServicesClient client(::grpc::CreateChannel(server_address_str,
+                                                        grpc::InsecureChannelCredentials()));
+
+      std::cout << "**[ VersionService RPC test:" << std::endl;
+      client.VersionService();
+      std::cout << "**[ InsertionPointService RPC test:" << std::endl;
+      client.InsertionPointService();
+    }
   
   return 0;
 }
